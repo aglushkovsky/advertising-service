9d8b535119981e8e1e5d1a3f99c3b80af0c984c9
Alexander Glushkov <lushmetry@yandex.ru>
2025-04-15 12:43:35 +0300

    initial commit

---
+++ src/test/java/io/github/aglushkovsky/advertisingservice/AdvertisingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aglushkovsky.advertisingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
