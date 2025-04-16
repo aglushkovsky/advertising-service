54f1cc16a817aa7e02e2b6a8f021b59d02cd0087
Alexander Glushkov <lushmetry@yandex.ru>
2025-04-16 20:23:51 +0300

    added jpa configuration class and implemented hibernate configuration with yaml-properties

---
+++ src/main/java/io/github/aglushkovsky/advertisingservice/config/JpaConfig.java
@@ -0,0 +1,60 @@
+package io.github.aglushkovsky.advertisingservice.config;
+
+import io.github.aglushkovsky.advertisingservice.config.property.HibernateProperties;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@AllArgsConstructor
+@EnableTransactionManagement
+public class JpaConfig {
+
+    private HibernateProperties hibernateProperties;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName(hibernateProperties.getDriver());
+        driverManagerDataSource.setUrl(hibernateProperties.getUrl());
+        driverManagerDataSource.setUsername(hibernateProperties.getUsername());
+        driverManagerDataSource.setPassword(hibernateProperties.getPassword());
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emfb = new LocalContainerEntityManagerFactoryBean();
+        emfb.setDataSource(dataSource());
+        emfb.setPackagesToScan(hibernateProperties.getEntityPackage());
+        emfb.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        emfb.setJpaProperties(getHibernateProperties());
+        return emfb;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.physical_naming_strategy", hibernateProperties.getNamingPhysicalStrategy());
+        properties.setProperty("hibernate.dialect", hibernateProperties.getDialect());
+        properties.setProperty("show_sql", hibernateProperties.getSql().getShow().toString());
+        properties.setProperty("format_sql", hibernateProperties.getSql().getFormat().toString());
+        return properties;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(emf);
+        return jpaTransactionManager;
+    }
+}
---
+++ src/main/java/io/github/aglushkovsky/advertisingservice/config/property/HibernateProperties.java
@@ -0,0 +1,25 @@
+package io.github.aglushkovsky.advertisingservice.config.property;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "hibernate")
+public class HibernateProperties {
+    private String username;
+    private String password;
+    private String url;
+    private String driver;
+    private String entityPackage;
+    private String namingPhysicalStrategy;
+    private String dialect;
+    private Sql sql;
+
+    @Data
+    public static class Sql {
+        private Boolean show;
+        private Boolean format;
+    }
+}
