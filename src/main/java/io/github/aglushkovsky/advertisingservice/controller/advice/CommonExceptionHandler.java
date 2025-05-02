2ce1f13317c533f89dd7fe32882276253903796d
Alexander Glushkov <lushmetry@yandex.ru>
2025-05-02 17:57:52 +0300

    some improves in CommonExceptionHandler

--- src/main/java/io/github/aglushkovsky/advertisingservice/controller/advice/CommonExceptionHandler.java
+++ src/main/java/io/github/aglushkovsky/advertisingservice/controller/advice/CommonExceptionHandler.java
@@ -20,8 +20,8 @@ public class CommonExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponseDto<String> handleNotFoundException() {
-        log.info("Start handling NotFoundException");
+    public ErrorResponseDto<String> handleNotFoundException(NotFoundException e) {
+        log.info("Start handling NotFoundException", e);
         ErrorResponseDto<String> response = new ErrorResponseDto<>(
                 HttpStatus.NOT_FOUND.value(),
                 "Элемент не найден");
@@ -32,7 +32,7 @@ public class CommonExceptionHandler {
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponseDto<List<String>> handleConstraintViolationException(ConstraintViolationException e) {
-        log.info("Start handling ConstraintViolationException");
+        log.info("Start handling ConstraintViolationException", e);
         ErrorResponseDto<List<String>> response = new ErrorResponseDto<>(
                 HttpStatus.BAD_REQUEST.value(),
                 e.getConstraintViolations().stream().map(ConstraintViolation::getMessage).toList()
@@ -44,7 +44,7 @@ public class CommonExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponseDto<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.info("Start handling MethodArgumentNotValidException");
+        log.info("Start handling MethodArgumentNotValidException", e);
         ErrorResponseDto<List<String>> response = new ErrorResponseDto<>(
                 HttpStatus.BAD_REQUEST.value(),
                 e.getBindingResult().getAllErrors()
@@ -53,6 +53,18 @@ public class CommonExceptionHandler {
                         .toList()
         );
         log.info("Finished handling MethodArgumentNotValidException");
+        return response;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponseDto<String> handleException(Exception e) {
+        log.info("Start handling Exception; was thrown {}", e, e);
+        ErrorResponseDto<String> response = new ErrorResponseDto<>(
+                HttpStatus.BAD_REQUEST.value(),
+                "Что-то пошло не так"
+        );
+        log.info("Finished handling Exception");
         return response;
     }
 }
