91d9b769940cc63c3d341b04a6d8023e79fa4d49
Alexander Glushkov <lushmetry@yandex.ru>
2025-05-03 18:16:53 +0300

    refactored AdMapper

--- src/main/java/io/github/aglushkovsky/advertisingservice/mapper/AdMapper.java
+++ src/main/java/io/github/aglushkovsky/advertisingservice/mapper/AdMapper.java
@@ -1,19 +1,18 @@
 package io.github.aglushkovsky.advertisingservice.mapper;
 
 import io.github.aglushkovsky.advertisingservice.dto.response.AdResponseDto;
-import io.github.aglushkovsky.advertisingservice.dto.LocalityDto;
 import io.github.aglushkovsky.advertisingservice.entity.Ad;
-import io.github.aglushkovsky.advertisingservice.entity.Locality;
-import io.github.aglushkovsky.advertisingservice.entity.LocalityPart;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import static org.mapstruct.MappingConstants.ComponentModel.*;
 
-@Mapper(componentModel = SPRING)
+@Mapper(componentModel = SPRING,
+        uses = LocalityMapper.class,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface AdMapper {
 
     int DIGITS_AFTER_POINT = 2;
@@ -21,18 +20,7 @@ public interface AdMapper {
     @Mapping(target = "localityParts", source = "locality")
     AdResponseDto toDto(Ad ad);
 
-    LocalityDto localityToLocalityDto(Locality locality);
-
     default BigDecimal convertToReadablePrice(BigDecimal price) {
         return price == null ? null : price.movePointLeft(DIGITS_AFTER_POINT);
-    }
-
-    default List<LocalityDto> localityAncestorsToLocalityDtoList(Locality locality) {
-        return locality.getAncestors()
-                .stream()
-                .sorted()
-                .map(LocalityPart::getAncestorLocality)
-                .map(this::localityToLocalityDto)
-                .toList();
     }
 }
--- src/main/java/io/github/aglushkovsky/advertisingservice/mapper/LocalityMapper.java
+++ src/main/java/io/github/aglushkovsky/advertisingservice/mapper/LocalityMapper.java
@@ -2,7 +2,10 @@ package io.github.aglushkovsky.advertisingservice.mapper;
 
 import io.github.aglushkovsky.advertisingservice.dto.LocalityDto;
 import io.github.aglushkovsky.advertisingservice.entity.Locality;
+import io.github.aglushkovsky.advertisingservice.entity.LocalityPart;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 import static org.mapstruct.MappingConstants.ComponentModel.*;
 
@@ -10,4 +13,13 @@ import static org.mapstruct.MappingConstants.ComponentModel.*;
 public interface LocalityMapper {
 
     LocalityDto toDto(Locality locality);
+
+    default List<LocalityDto> localityAncestorsToLocalityDtoList(Locality locality) {
+        return locality.getAncestors()
+                .stream()
+                .sorted()
+                .map(LocalityPart::getAncestorLocality)
+                .map(this::toDto)
+                .toList();
+    }
 }
