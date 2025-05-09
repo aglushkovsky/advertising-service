f35ff5301e86eab29229a3c378709a5b1fbb9d3c
Alexander Glushkov <lushmetry@yandex.ru>
2025-05-09 19:40:23 +0300

    added integration tests for controllers

---
+++ src/test/java/io/github/aglushkovsky/advertisingservice/integration/AbstractIntegrationTest.java
@@ -0,0 +1,31 @@
+package io.github.aglushkovsky.advertisingservice.integration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static io.github.aglushkovsky.advertisingservice.config.property.HibernateProperties.*;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+abstract public class AbstractIntegrationTest {
+
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:16.8");
+
+    @BeforeAll
+    static void runContainer() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void postgres(DynamicPropertyRegistry registry) {
+        registry.add(URL_KEY, container::getJdbcUrl);
+    }
+}
