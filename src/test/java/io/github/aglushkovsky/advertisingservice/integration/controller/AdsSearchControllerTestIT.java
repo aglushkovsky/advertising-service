e1dd41ffaf44e0ce26f581a626ee2ab77412ff13
Alexander Glushkov <lushmetry@yandex.ru>
2025-05-18 20:42:37 +0300

    refactored AdsSearchControllerTestIT

--- src/test/java/io/github/aglushkovsky/advertisingservice/integration/controller/AdsSearchControllerTestIT.java
+++ src/test/java/io/github/aglushkovsky/advertisingservice/integration/controller/AdsSearchControllerTestIT.java
@@ -1,8 +1,9 @@
 package io.github.aglushkovsky.advertisingservice.integration.controller;
 
 import io.github.aglushkovsky.advertisingservice.integration.AbstractIntegrationTest;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,47 +17,41 @@ public class AdsSearchControllerTestIT extends AbstractIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Nested
-    class SearchAds {
+    @ParameterizedTest
+    @ValueSource(strings = {"Macbook", "macbook", "Macbook pro", "Macbook Pro", "macbook Pro"})
+    void searchAdsShouldReturnResultWhenParametersIsValid(String searchTerm) throws Exception {
+        Integer publisherId = 1;
+        Integer localityId = 1;
+        Integer page = 1;
 
-        @Test
-        void searchAdsShouldReturnResultWhenParametersIsValid() throws Exception {
-            String searchTerm = "macbook";
-            Integer publisherId = 1;
-            Integer localityId = 1;
-            Integer page = 1;
+        mockMvc.perform(get("/api/v1/ads")
+                        .queryParam("term", searchTerm)
+                        .queryParam("onlyInTitle", "1")
+                        .queryParam("minPrice", "10000000")
+                        .queryParam("maxPrice", "20000000")
+                        .queryParam("localityId", localityId.toString())
+                        .queryParam("publisherId", publisherId.toString())
+                        .queryParam("page", page.toString())
+                        .queryParam("limit", "10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.body.size()").value(1))
+                .andExpect(jsonPath("$.body[0].title", containsStringIgnoringCase(searchTerm)))
+                .andExpect(jsonPath("$.body[0].publisher.id", is(publisherId)))
+                .andExpect(jsonPath("$.body[0].localityParts[*].id", hasItem(localityId)))
+                .andExpect(jsonPath("$.metadata.currentPage").value(1))
+                .andExpect(jsonPath("$.metadata.totalPages").value(page))
+                .andExpect(jsonPath("$.metadata.totalRecords").value(1))
+                .andExpect(jsonPath("$.metadata.isLastPage").value(true));
+    }
 
-            mockMvc.perform(get("/api/v1/ads")
-                            .queryParam("term", searchTerm)
-                            .queryParam("onlyInTitle", "true")
-                            .queryParam("minPrice", "10000000")
-                            .queryParam("maxPrice", "20000000")
-                            .queryParam("localityId", localityId.toString())
-                            .queryParam("publisherId", publisherId.toString())
-                            .queryParam("page", page.toString())
-                            .queryParam("limit", "10"))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.body.size()").value(1))
-                    .andExpect(jsonPath("$.body[0].title", containsStringIgnoringCase(searchTerm)))
-                    .andExpect(jsonPath("$.body[0].publisher.id", is(publisherId)))
-                    .andExpect(jsonPath("$.body[0].localityParts[*].id", hasItem(localityId)))
-                    .andExpect(jsonPath("$.metadata.currentPage").value(1))
-                    .andExpect(jsonPath("$.metadata.totalPages").value(page))
-                    .andExpect(jsonPath("$.metadata.totalRecords").value(1))
-                    .andExpect(jsonPath("$.metadata.isLastPage").value(true));
-        }
+    @Test
+    void getAdsHistoryByUserIdShouldReturnResultWhenParametersIsValid() throws Exception {
+        Long userId = 1L;
 
-        @Test
-        void searchAdsShouldReturnErrorResponseWhenFilterParametersIsInvalid() throws Exception {
-            mockMvc.perform(get("/api/v1/ads")
-                            .queryParam("minPrice", "-10000000")
-                            .queryParam("maxPrice", "-20000000")
-                            .queryParam("localityId", "0")
-                            .queryParam("publisherId", "0"))
-                    .andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("$.body.size()").value(5))
-                    .andExpect(jsonPath("$.body[*].parameter",
-                            hasItems("minPrice", "maxPrice", "localityId", "publisherId", null)));
-        }
+        mockMvc.perform(get("/api/v1/users/{0}/ads/history", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.body.size()").value(1))
+                .andExpect(jsonPath("$.body[0].publisher.id", is(1)))
+                .andExpect(jsonPath("$.body[0].status", is("SOLD")));
     }
 }
