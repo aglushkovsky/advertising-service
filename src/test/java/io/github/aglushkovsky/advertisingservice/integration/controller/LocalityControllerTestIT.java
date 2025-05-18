e9640fbba39fc2c36d7f4ea1947f7b1fbaf40403
Alexander Glushkov <lushmetry@yandex.ru>
2025-05-18 19:51:03 +0300

    refactored LocalityControllerTestIT, removed unnecessary tests

--- src/test/java/io/github/aglushkovsky/advertisingservice/integration/controller/LocalityControllerTestIT.java
+++ src/test/java/io/github/aglushkovsky/advertisingservice/integration/controller/LocalityControllerTestIT.java
@@ -2,10 +2,8 @@ package io.github.aglushkovsky.advertisingservice.integration.controller;
 
 import io.github.aglushkovsky.advertisingservice.entity.enumeration.LocalityType;
 import io.github.aglushkovsky.advertisingservice.integration.AbstractIntegrationTest;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.*;
@@ -17,52 +15,24 @@ public class LocalityControllerTestIT extends AbstractIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Nested
-    class FindAllByLocalityType {
-
-        @Test
-        void findAllByLocalityTypeShouldReturnResult() throws Exception {
-            mockMvc.perform(get("/api/v1/localities"))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.size()").value(3))
-                    .andExpect(jsonPath("$.[*].name", hasItems("Орёл", "Мценск", "Брянск")))
-                    .andExpect(jsonPath("$.[*].type", everyItem(is(LocalityType.CITY.name()))));
-        }
-
-        @Test
-        void findAllByLocalityTypeShouldReturnErrorResponseWhenLocalityTypeIsInvalid() throws Exception {
-            String invalidLocalityType = "invalid";
-
-            mockMvc.perform(get("/api/v1/localities?type={0}", invalidLocalityType))
-                    .andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
-                    .andExpect(jsonPath("$.errors.size()").value(1));
-        }
+    @Test
+    void findAllByLocalityTypeShouldReturnResult() throws Exception {
+        mockMvc.perform(get("/api/v1/localities"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(3))
+                .andExpect(jsonPath("$.[*].name", hasItems("Орёл", "Мценск", "Брянск")))
+                .andExpect(jsonPath("$.[*].type", everyItem(is(LocalityType.CITY.name()))));
     }
 
-    @Nested
-    class FindDirectDescendantsByLocalityId {
+    @Test
+    void findDirectDescendantsByLocalityId() throws Exception {
+        Long localityId = 1L;
 
-        @Test
-        void findDirectDescendantsByLocalityId() throws Exception {
-            Long localityId = 1L;
-
-            mockMvc.perform(get("/api/v1/localities/{0}/descendants", localityId))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.size()").value(4))
-                    .andExpect(jsonPath("$.[*].name",
-                            hasItems("Советский", "Железнодорожный", "Заводской", "Северный")))
-                    .andExpect(jsonPath("$.[*].type", everyItem(is(LocalityType.DISTRICT.name()))));
-        }
-
-        @Test
-        void findDirectDescendantsByLocalityIdShouldReturnErrorResponseWhenLocalityIdIsInvalid() throws Exception {
-            Long invalidLocalityId = 0L;
-
-            mockMvc.perform(get("/api/v1/localities/{0}/descendants", invalidLocalityId))
-                    .andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
-                    .andExpect(jsonPath("$.errors.size()").value(1));
-        }
+        mockMvc.perform(get("/api/v1/localities/{0}/descendants", localityId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(4))
+                .andExpect(jsonPath("$.[*].name",
+                        hasItems("Советский", "Железнодорожный", "Заводской", "Северный")))
+                .andExpect(jsonPath("$.[*].type", everyItem(is(LocalityType.DISTRICT.name()))));
     }
 }
