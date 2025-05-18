b110b7e72ec9ac375f4739519f21f0880e2731ac
Alexander Glushkov <lushmetry@yandex.ru>
2025-05-18 23:55:25 +0300

    refactored AdCrudControllerTestIT and LocalityControllerTestIT

--- src/test/java/io/github/aglushkovsky/advertisingservice/integration/controller/LocalityControllerTestIT.java
+++ src/test/java/io/github/aglushkovsky/advertisingservice/integration/controller/LocalityControllerTestIT.java
@@ -2,8 +2,10 @@ package io.github.aglushkovsky.advertisingservice.integration.controller;
 
 import io.github.aglushkovsky.advertisingservice.entity.enumeration.LocalityType;
 import io.github.aglushkovsky.advertisingservice.integration.AbstractIntegrationTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.*;
@@ -17,22 +19,36 @@ public class LocalityControllerTestIT extends AbstractIntegrationTest {
 
     @Test
     void findAllByLocalityTypeShouldReturnResult() throws Exception {
-        mockMvc.perform(get("/api/v1/localities"))
+        mockMvc.perform(get("/api/v1/localities")
+                        .queryParam("type", LocalityType.CITY.name()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()").value(3))
                 .andExpect(jsonPath("$.[*].name", hasItems("Орёл", "Мценск", "Брянск")))
                 .andExpect(jsonPath("$.[*].type", everyItem(is(LocalityType.CITY.name()))));
     }
 
-    @Test
-    void findDirectDescendantsByLocalityId() throws Exception {
-        Long localityId = 1L;
+    @Nested
+    class FindDirectDescendantsByLocalityId {
 
-        mockMvc.perform(get("/api/v1/localities/{0}/descendants", localityId))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()").value(4))
-                .andExpect(jsonPath("$.[*].name",
-                        hasItems("Советский", "Железнодорожный", "Заводской", "Северный")))
-                .andExpect(jsonPath("$.[*].type", everyItem(is(LocalityType.DISTRICT.name()))));
+        @Test
+        void findDirectDescendantsByLocalityIdShouldReturnResultWhenLocalityIdIsExists() throws Exception {
+            Long localityId = 1L;
+
+            mockMvc.perform(get("/api/v1/localities/{0}/descendants", localityId))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.size()").value(4))
+                    .andExpect(jsonPath("$.[*].name",
+                            hasItems("Советский", "Железнодорожный", "Заводской", "Северный")))
+                    .andExpect(jsonPath("$.[*].type", everyItem(is(LocalityType.DISTRICT.name()))));
+        }
+
+        @Test
+        void findDirectDescendantsByLocalityIdShouldReturnNotFoundResponseWhenLocalityIdDoesNotExists() throws Exception {
+            Long localityId = 1000L;
+
+            mockMvc.perform(get("/api/v1/localities/{0}/descendants", localityId))
+                    .andExpect(status().isNotFound())
+                    .andExpect(jsonPath("$.status").value(HttpStatus.NOT_FOUND.value()));
+        }
     }
 }
